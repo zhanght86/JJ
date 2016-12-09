91eeaae3b62a3286ecfaacabe29393ebaab56048
panxiong <303507479@163.com>
2016-12-09 20:13:57 +0800

    添加server

---
+++ mssql/db.js
@@ -0,0 +1,47 @@
+﻿//封装连接数据库代码:db.js
+//mssql模块简单封装 
+var mssql=require('mssql');
+var db={};
+var config={
+	user:'nodejs',
+	password:'nodejs',
+	server:'192.168.2.207',
+	database:'nodejs',
+	//port:123,
+	options:{
+		//encrypt:true
+	},
+	pool:{
+		min:0,max:10, idleTimeoutMillis: 3000
+	}
+};
+db.sql=function(sql,callback){
+	var connection=new mssql.Connection(config,function(err){
+		if(err){
+			console.log(err);
+			return;
+		}
+		var ps=new mssql.PreparedStatement(connection);
+		ps.prepare(sql,function(err){
+			if(err){
+				console.log(err);
+				return;
+			}
+			ps.execute('',function(err,result){
+				if(err){
+					console.log(err);
+					return;
+				}
+				ps,unprepare(function(err){
+					if(err){
+						console.log(err);
+						callback(err,null);
+						return;
+					}
+					callback(err,result);
+				});
+			});
+		});
+	});
+};
+module.exports=db;

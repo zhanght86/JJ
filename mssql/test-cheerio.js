38a03ddd7088600d1392440fa1c7f05b15cca960
panxiong <303507479@163.com>
2016-12-12 18:24:59 +0800

    爬虫一波

---
+++ mssql/test-cheerio.js
@@ -0,0 +1,5 @@
+﻿var reptile=require('./part/cheerio-reptile');
+var url = "http://www.ss.pku.edu.cn/index.php/newscenter/news/2391";
+reptile.fetchPage("http://www.baidu.com", function ($) {
+    console.log($);
+});
---
+++ mssql/test-reptile.js
@@ -0,0 +1,5 @@
+﻿var $=require('./part/jq-reptile').jq();
+$.get("http://www.baidu.com", "gbk", function (html) {
+    var title = html.find("title").text();
+    console.log(title);
+});

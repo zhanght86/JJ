b1514ef45f35e52b19224b141b863c1050381527
panxiong <303507479@163.com>
2017-02-04 10:06:02 +0800

    把一些常用的前端效果放上去

---
+++ front/资源/百度地图资源/api.js
@@ -0,0 +1,1 @@
+(function(){window.wise=1;window.netSpeed=370;window.netType=3; window.BMap_loadScriptTime = (new Date).getTime(); document.write('<script type="text/javascript" src="http://api.map.baidu.com/getscript?v=1.3&ak=&services=&t=20150527115231"></script>');document.write('<link rel="stylesheet" type="text/css" href="http://api.map.baidu.com/res/13/bmap.css"/>');})();

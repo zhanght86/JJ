05fd909f5d95e4dd95941a08c21e1f316263d34b
panxiong <panxiong@its-cn.com>
2017-03-28 14:47:43 +0800

    过滤掉node-module文件夹

--- mssql/test-reptile.js
+++
@@ -1,5 +0,0 @@
-﻿var $=require('./part/jq-reptile').jq();
-$.get("http://www.baidu.com", "gbk", function (html) {
-    var title = html.find("title").text();
-    console.log(title);
-});
--- mssql/test.js
+++
@@ -1,8 +0,0 @@
-﻿var db=require('./db');
-db.sql('select * from student',function(err,result){
-	if(err){
-		console.log(err);
-		return;
-	}
-	console.log('学生总数为：',result.length);
-});
